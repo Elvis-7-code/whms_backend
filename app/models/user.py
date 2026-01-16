034e96f5213f3245a6031a00faf7918242ecb2c1
Elvis <elvis.wachira@student.moringaschool.com>
2026-01-16 21:57:14 +0300

    protect_routes_user.py

--- app/models/user.py
+++ app/models/user.py
@@ -9,3 +9,9 @@ class User(db.Model):
     password_hash = db.Column(db.String(255))
     role = db.Column(db.String(20))  # owner, manager, worker
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)   

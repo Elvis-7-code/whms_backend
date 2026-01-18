ac095256f34200034739e3d20499371595ebebf0
Elvis <elvis.wachira@student.moringaschool.com>
2026-01-18 17:53:15 +0300

    breeding.py_day12

--- app/models/breeding.py
+++ app/models/breeding.py
@@ -1,10 +1,11 @@
+from datetime import timedelta
 from app.extensions import db
 
 class Breeding(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    bull_id = db.Column(db.Integer, db.ForeignKey("animal.id"))
-    cow_id = db.Column(db.Integer, db.ForeignKey("animal.id"))
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
-    expected_birth = db.Column(db.Date)
-    status = db.Column(db.String(20))  # active, completed
+    male_id = db.Column(db.Integer, db.ForeignKey("animal.id"), nullable=False)
+    female_id = db.Column(db.Integer, db.ForeignKey("animal.id"), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=True)
+    expected_birth = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(20), default='pregnant')  # active, completed

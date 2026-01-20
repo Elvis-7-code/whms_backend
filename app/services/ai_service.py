b8968182bc77a069f59ceb3f2f9ef847bd17447c
Elvis <elvis.wachira@student.moringaschool.com>
2026-01-20 17:58:49 +0300

    AI implementation

---
+++ app/services/ai_service.py
@@ -0,0 +1,9 @@
+"""
+AI-related logic(failure)
+Predictions, recommendations, anomaly detection
+"""
+
+def predict_feed_usage(data):
+    return{
+        'prediction':'AI feature coming soon'
+    }
